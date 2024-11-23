b8d0d248b4f6561eb8859a2be9fd732ed9900e94
Oscar Usuga <v-ousuga@expediagroup.com>
2024-11-23 08:57:52 -0500

    Adding integration to generate prompts considering the chat history

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/controller/ChatBotController.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/controller/ChatBotController.java
@@ -2,6 +2,7 @@ package com.epam.training.gen.ai.controller;
 
 import com.epam.training.gen.ai.model.ChatBotResponse;
 import com.epam.training.gen.ai.model.Prompt;
+import com.epam.training.gen.ai.service.SemanticKernelService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class ChatBotController {
 
+    private SemanticKernelService semanticKernelService;
+
     @PostMapping("chat-bot/prompt")
     public ChatBotResponse promptChatBot(@RequestBody Prompt request) {
-        return new ChatBotResponse("response");
+        var response = semanticKernelService.processPrompt(request.input());
+        return new ChatBotResponse(response);
     }
 }
--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/service/SemanticKernelService.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/service/SemanticKernelService.java
@@ -1,9 +1,14 @@
 package com.epam.training.gen.ai.service;
 
 import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunction;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunctionArguments;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class SemanticKernelService {
@@ -11,6 +16,29 @@ public class SemanticKernelService {
     private final Kernel kernel;
 
     public String processPrompt(String prompt) {
-        return null;
+        var chatHistory = new ChatHistory();
+        var response = kernel.invokeAsync(getChat())
+                .withArguments(getKernelFunctionArguments(prompt, chatHistory))
+                .block();
+
+        chatHistory.addUserMessage(prompt);
+        chatHistory.addAssistantMessage(response.getResult());
+
+        log.info("AI answer: " + response.getResult());
+        return response.getResult();
+    }
+
+    private KernelFunction<String> getChat() {
+        return KernelFunction.<String>createFromPrompt("""
+                    {{$chatHistory}}
+                    <message role="user>{{$request}}</message>""")
+                .build();
+    }
+
+    private KernelFunctionArguments getKernelFunctionArguments(String prompt, ChatHistory chatHistory) {
+        return KernelFunctionArguments.builder()
+                .withVariable("request", prompt)
+                .withVariable("chatHistory", chatHistory)
+                .build();
     }
 }
