cb68b7eda4eade619a2457cb398ab51e4a4a795e
Oscar Usuga <v-ousuga@expediagroup.com>
2024-11-27 21:41:00 -0500

    Adding PromptExecutionSettings to configure temperature

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/configuration/SemanticKernelConfiguration.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/configuration/SemanticKernelConfiguration.java
@@ -3,6 +3,7 @@ package com.epam.training.gen.ai.configuration;
 import com.azure.ai.openai.OpenAIAsyncClient;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
+import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +25,15 @@ public class SemanticKernelConfiguration {
     public Kernel kernel(ChatCompletionService chatCompletionService) {
         return Kernel.builder()
                 .withAIService(ChatCompletionService.class, chatCompletionService)
+                .build();
+    }
+
+    @Bean
+    public PromptExecutionSettings promptExecutionSettings(@Value("${client.prompt.max-tokens}") int maxTokens,
+                                                           @Value("${client.prompt.temperature}") double temperature) {
+        return PromptExecutionSettings.builder()
+                .withMaxTokens(maxTokens)
+                .withTemperature(temperature)
                 .build();
     }
 }
